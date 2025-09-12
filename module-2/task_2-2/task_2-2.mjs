a7bafaf1f55c614dae9df9ed5609e48fea55aaed
Thevasenan <senan27tt1@gmail.com>
2025-09-12 15:34:10 +0200

    For å få 100 linjer

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -92,7 +92,8 @@ printOut(newLine);
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 let sporDay= 131; //Calculating days and weeks from 131 days
-let weeks=1*7;
+let day=1;
+let weeks=day*7;
 let convWeeks= Math.floor(sporDay/weeks);
 let convDays= Math.floor(sporDay%weeks);
 printOut("131 dager er "+ convWeeks + " uker og "+ convDays + " dager");
