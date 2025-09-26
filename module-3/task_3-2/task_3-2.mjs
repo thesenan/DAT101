43f7ce288e666673ea5bca2dfb78f3dfa51c9f2f
Thevasenan <senan27tt1@gmail.com>
2025-09-26 15:13:13 +0200

    Jobber med task 3.2

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,32 +3,104 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+// For loops that counts up to 10 and down to 0. That stores and prints the values in a string
+let txtPrintValue= "";
+let txtPrintValue2= "";
+
+for (let i=1,j=10;i<=10, j>=1;i++,j--){
+    txtPrintValue+=i.toString()+" ";
+    txtPrintValue2+=j.toString()+" ";
+}
+
+printOut(txtPrintValue+"<br>"+txtPrintValue2);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+// To variables that generates a random number between 1 and 60. A while loop that compares the two variables until they are the same. The While loop printed every guess until the correct number is guessed.
+let txtPrintValue3= "";
+let guessNumberAns= Math.floor(Math.random()*60)+1;
+let guessNumberMachine= Math.floor(Math.random()*60)+1;
+
+while(guessNumberMachine!==guessNumberAns){
+    guessNumberMachine= Math.floor(Math.random()*60)+1;
+    txtPrintValue3+=guessNumberMachine.toString()+" ";
+}
+
+printOut("Svaret er: "+guessNumberAns+ "<br> Maskinen gjetter: " +txtPrintValue3);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+// Similar to part 2.But whit larger range of numbers. The while loop has a counter that counts how many guesses the machine needs to find the correct number. And it measures the time it takes to find the corrects number.
+const timeStart= Date.now();
+let txtPrintValue4= "";
+let guessCount=0;
+let guessNumberAns2= Math.floor(Math.random()*1000000)+1;
+let guessNumberMachine2= Math.floor(Math.random()*1000000)+1;
+
+while(guessNumberMachine2!==guessNumberAns2){
+    guessNumberMachine2= Math.floor(Math.random()*1000000)+1;
+    guessCount++;
+    txtPrintValue4=guessCount.toString();
+}
+
+const timeEnd= Date.now();
+const timeUsed= (timeEnd-timeStart);
+
+printOut("Svaret er: "+guessNumberAns2+"<br> Maskinen: "+txtPrintValue4+" ganger for å gjette tallet"+"<br> Maskinen bruker "+ timeUsed+ "ms på å finne svaret.");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let txtPrintValue5= "";
+let isPrime= true;
+let pCheck2="";
+
+for(let count=2;count<200;count++){
+    pCheck2= count%2;
+    if (pCheck2===0){
+        isPrime=false;
+    }
+    while(isPrime===true){
+        let guessPrime=count;
+        txtPrintValue5+=guessPrime.toString();
+        printOut(txtPrintValue5);
+    }
+}
+
+printOut(txtPrintValue5);
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const pRow=7;
+const pColum=9;
+let txtPrintValue6="";
+
+for (let i=1;i<=pRow;i++){
+    for (let j=1;j<=pColum;j++){
+        txtPrintValue6+= " K"+j.toString()+"R"+i.toString();
+    }
+    txtPrintValue6+="<br>";
+}
+
+printOut(txtPrintValue6);
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let txtPrintValue8="";
+
+for (let i=1;i<=5;i++){
+    while(i!==5){
+    gradeR=Math.floor(Math.random()*126+1);
+    txtPrintValue8+=" "+gradeR.toString();
+}
+}
+
+
+printOut(txtPrintValue8);
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
