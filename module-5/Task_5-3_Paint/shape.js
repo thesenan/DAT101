962c9b164f0c7b9b1e66843b992cd3df7ac81c55
thevasenat <senan27tt1@gmail.com>
2026-02-10 15:54:47 +0100

    Update

--- module-5/Task_5-3_Paint/shape.js
+++ module-5/Task_5-3_Paint/shape.js
@@ -6,15 +6,77 @@ const cvsPaint = document.getElementById("cvsPaint");
 const ctxPaint = cvsPaint.getContext("2d");
 
 let mousePos = new TPoint();
+let shape = null;
+let shapes = [];
+
+class TShape{
+
+    constructor(aX, aY){
+        this.posStart = new TPoint(aX, aY);
+        this.posEnd = null;
+    }
+
+    setEndPos(aX, aY){
+        this.posEnd = new TPoint(aX, aY);
+    }
+
+    draw(){
+        ctxPaint.beginPath();
+        ctxPaint.moveTo(this.posStart.x, this.posStart.y);
+        if(this.posEnd){
+            ctxPaint.lineTo(this.posEnd.x, this.posEnd.y);
+        }else{
+            ctxPaint.lineTo(mousePos.x, mousePos.y);
+        }
+        ctxPaint.stroke();
+    }
+}
 
 function updateMousePos(aEvent){
     const rect = cvsPaint.getBoundingClientRect();
+    mousePos.x = Math.round(aEvent.clientX - rect.left);
+    mousePos.y = Math.round(aEvent.clientY - rect.top);
 }
 
 function mouseDown(aEvent){
-    const x = aEvent.x;
-    const y = aEvent.y;
-    console.log(`Mouse Down at: x=${x}, y=${y}`);
+    updateMousePos(aEvent);
+    const x = mousePos.x;
+    const y = mousePos.y;
+    if(shape === null){
+        shape = new TShape(mousePos.x, mousePos.y);
+    }
+}
+
+function mouseMove(aEvent){
+    updateMousePos(aEvent);
+}
+
+function mouseUp(aEvent){
+    updateMousePos(aEvent);
+    if(shape){
+        shape.setEndPos(mousePos.x, mousePos.y);
+        shapes.push(shape);
+        shape = null;
+
+    }
+    
+}
+
+function drawCanvas(){
+    ctxPaint.clearRect(0, 0, cvsPaint.clientWidth, cvsPaint.height);
+    if(shape){
+        shape.draw();
+    }
+    for(let i = 0; i < shapes.length; i++){
+        shapes[i].draw();
+    }
+    /*shapes.forEach(element => {
+        element.draw();
+    });*/
+    requestAnimationFrame(drawCanvas);
 }
 
 cvsPaint.addEventListener("mousedown", mouseDown);
+cvsPaint.addEventListener("mouseup", mouseUp);
+cvsPaint.addEventListener("mousemove", mouseMove);
+drawCanvas();
