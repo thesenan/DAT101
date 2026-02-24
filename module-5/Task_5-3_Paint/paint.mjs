0ba6b913de958d66b53d769afe8412c4f877b9d8
thevasenat <senan27tt1@gmail.com>
2026-02-24 12:44:19 +0100

    update

--- module-5/Task_5-3_Paint/paint.mjs
+++ module-5/Task_5-3_Paint/paint.mjs
@@ -87,7 +87,7 @@ You need to build this file with the following features:
  *    When user clicks a menu button (e.g., "Circle"), this object gets updated
  *    So when drawing a new shape, the app knows which settings to use
  */
-const newShapeType = { ShapeType: EShapeType.Line, FillColor: EColorType.Black, StrokeColor: EColorType.Black, StrokeSize: EStrokeSizeType.Thin };
+export const newShapeType = { ShapeType: EShapeType.Line, FillColor: EColorType.Black, StrokeColor: EColorType.Black, StrokeSize: EStrokeSizeType.Thin };
 
 /**
  * 🎮 CREATE THE MENU TOOLBAR
--- module-5/Task_5-3_Paint/shape.js
+++ module-5/Task_5-3_Paint/shape.js
@@ -1,5 +1,7 @@
 "use strict";
 import { TPoint } from "lib2d";
+import { newShapeType } from "./paint.mjs";
+import { EShapeType } from "./menu.js";
 
 // 🖼️ Access the paint canvas and drawing context, move this to your new JavaScript file.
 const cvsPaint = document.getElementById("cvsPaint");
@@ -21,13 +23,140 @@ class TShape{
     }
 
     draw(){
+    }
+}
+
+
+export class TLineShape extends TShape{
+
+    constructor(aX, aY){
+        super(aX, aY);
+    }
+    
+    draw(){
         ctxPaint.beginPath();
         ctxPaint.moveTo(this.posStart.x, this.posStart.y);
-        if(this.posEnd){
-            ctxPaint.lineTo(this.posEnd.x, this.posEnd.y);
-        }else{
-            ctxPaint.lineTo(mousePos.x, mousePos.y);
+            if(this.posEnd){
+                ctxPaint.lineTo(this.posEnd.x, this.posEnd.y);
+            }else{
+                ctxPaint.lineTo(mousePos.x, mousePos.y);
+            }
+        ctxPaint.stroke();
+    }
+}
+
+export class TCircleShape extends TShape{
+    #radius;
+
+    constructor(aX, aY){
+        super(aX, aY);
+        this.#radius = 0;
+    }
+    
+    draw(){
+        ctxPaint.beginPath();
+        //ctxPaint.moveTo(this.posStart.x, this.posStart.y);
+            if(!this.posEnd){
+                this.#calcRadius();
+            }
+
+            ctxPaint.arc(this.posStart.x, this.posStart.y, this.#radius, 0, 2*Math.PI);
+            ctxPaint.stroke();
+    }
+
+    #calcRadius(){
+        const dx = mousePos.x - this.posStart.x;
+        const dy = mousePos.y - this.posStart.y;
+        let hyp = Math.pow(dx,2) + Math.pow(dy,2);
+        hyp = Math.sqrt(hyp);
+        this.#radius = hyp;
+    }
+}
+
+export class TEllipseShape extends TShape{
+    #radius1;
+    #radius2;
+
+    constructor(aX, aY){
+        super(aX, aY);
+        this.#radius1 = 0;
+        this.#radius2 = 0;
+    }
+    
+    draw(){
+        ctxPaint.beginPath();
+        //ctxPaint.moveTo(this.posStart.x, this.posStart.y);
+            if(!this.posEnd){
+                this.#calcRadius();
+            }
+            ctxPaint.ellipse(
+                this.posStart.x, this.posStart.y,
+                this.#radius1, this.#radius2, 0, 0, 2*Math.PI);
+            ctxPaint.stroke();
+    }
+
+    #calcRadius(){
+        const dx = mousePos.x - this.posStart.x;
+        const dy = mousePos.y - this.posStart.y;
+        //let hyp = Math.pow(dx,2) + Math.pow(dy,2);
+        //hyp = Math.sqrt(hyp);
+        this.#radius1 = Math.abs(dx);
+        this.#radius2 = Math.abs(dy);
+    }
+}
+
+export class TRectangleShape extends TShape{
+    #width;
+    #height;
+
+    constructor(aX, aY){
+        super(aX, aY);
+        this.#width = 0;
+        this.#height = 0;
+        
+    }
+
+    draw(){
+        ctxPaint.beginPath();
+        ctxPaint.moveTo(this.posStart.x, this.posStart.y);
+            if(!this.posEnd){
+                this.#calcSize();
+            }
+
+            ctxPaint.rect(this.posStart.x, this.posStart.y, this.#width, this.#height);
+            ctxPaint.stroke();
+    }
+    #calcSize(){
+            this.#width = mousePos.x - this.posStart.x;
+            this.#height = mousePos.y - this.posStart.y;
+    }
+}
+
+export class TPenShape extends TShape{
+    #points;
+
+    constructor(aX, aY){
+        super(aX, aY);
+        this.#points = [];
+    }
+    
+    addPos(aX, aY){
+        const pos = new TPoint(aX, aY);
+        this.#points.push(pos);
+    }
+
+    draw(){
+        ctxPaint.beginPath();
+        ctxPaint.moveTo(this.posStart.x, this.posStart.y);
+
+        for(let i = 0; i < this.#points.length; i++){
+            const pos = this.#points[i];
+            ctxPaint.lineTo(pos.x, pos.y);
         }
+            if(this.posEnd){
+                ctxPaint.lineTo(this.posEnd.x, this.posEnd.y);
+            }
+
         ctxPaint.stroke();
     }
 }
@@ -36,6 +165,9 @@ function updateMousePos(aEvent){
     const rect = cvsPaint.getBoundingClientRect();
     mousePos.x = Math.round(aEvent.clientX - rect.left);
     mousePos.y = Math.round(aEvent.clientY - rect.top);
+    if((shape !== null) && (shape.addPos)){
+        shape.addPos(mousePos.x, mousePos.y);
+    }
 }
 
 function mouseDown(aEvent){
@@ -43,12 +175,30 @@ function mouseDown(aEvent){
     const x = mousePos.x;
     const y = mousePos.y;
     if(shape === null){
-        shape = new TShape(mousePos.x, mousePos.y);
+        switch(newShapeType.ShapeType){
+            case EShapeType.Line:
+                shape = new TLineShape(mousePos.x, mousePos.y);
+                break;
+            case EShapeType.Circle:
+                shape = new TCircleShape(mousePos.x, mousePos.y);
+                break;
+            case EShapeType.Oval:
+                shape = new TEllipseShape(mousePos.x, mousePos.y);
+                break;
+            case EShapeType.Rectangle:
+                shape = new TRectangleShape(mousePos.x, mousePos.y);
+                break;
+            case EShapeType.Pen:
+                shape = new TPenShape(mousePos.x, mousePos.y);
+            break;
+
+        }
     }
 }
 
 function mouseMove(aEvent){
     updateMousePos(aEvent);
+
 }
 
 function mouseUp(aEvent){
