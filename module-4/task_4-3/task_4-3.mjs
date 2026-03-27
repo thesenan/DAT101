eb720f2b258da600dc804f89db077979f632c66d
thevasenat <senan27tt1@gmail.com>
2026-03-28 00:29:54 +0100

    update

--- module-4/task_4-3/task_4-3.mjs
+++ module-4/task_4-3/task_4-3.mjs
@@ -48,8 +48,58 @@ const MovieGenre = [
 //--- Part 1 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
 
+const txtRectWidth = document.getElementById("txtRectWidth");
+const txtRectHeight = document.getElementById("txtRectHeight");
+const cmbTask1Calculate = document.getElementById("cmbTask1Calculate");
+const txtTask1Output = document.getElementById("txtTask1Output");
+
+function cmbTask1CalculateClick() {
+  let width = parseFloat(txtRectWidth.value);
+  let height = parseFloat(txtRectHeight.value);
+
+  // Validation
+  if (isNaN(width) || isNaN(height)) {
+    txtTask1Output.innerHTML = "Please enter valid numbers!";
+    return;
+  }
+
+  let circumference = 2 * (width + height);
+  let area = width * height;
+
+  txtTask1Output.innerHTML = "Circumference = " + circumference + ", Area = " + area;
+}
+
+cmbTask1Calculate.addEventListener("click", cmbTask1CalculateClick);
+
 //--- Part 2 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+
+const txtTask2Word = document.getElementById("txtTask2Word");
+const txtTask2Output = document.getElementById("txtTask2Output");
+
+let task2Words = [];
+
+function txtTask2WordKeyPress(aEvent) {
+  if (aEvent.key === "Enter") {
+    let word = txtTask2Word.value.trim();
+
+    if (word !== "") {
+      task2Words.push(word);
+    }
+
+    txtTask2Word.value = "";
+
+    let text = "Number of words = " + task2Words.length + "<br>";
+
+    for (let i = 0; i < task2Words.length; i++) {
+      text += i + 1 + ": " + task2Words[i] + "<br>";
+    }
+
+    txtTask2Output.innerHTML = text;
+  }
+}
+
+txtTask2Word.addEventListener("keypress", txtTask2WordKeyPress);
 
 //--- Part 3 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
@@ -58,10 +108,10 @@ const chkTask3 = document.getElementsByName("chkTask3");
 const cmbTask3CheckAnswer = document.getElementById("cmbTask3CheckAnswer");
 const txtTask3Output = document.getElementById("txtTask3Output");
 
-function cmbTask3CheckAnswerClick (){
+function cmbTask3CheckAnswerClick() {
   txtTask3Output.innerHTML = "";
 
-  for (let i = 0; i < chkTask3.length; i++){
+  for (let i = 0; i < chkTask3.length; i++) {
     const chkBox = chkTask3[i];
     const text = `chkTask3[${i}].checked = ${chkBox.checked}`;
     txtTask3Output.innerHTML += text + "</br>";
@@ -72,18 +122,16 @@ cmbTask3CheckAnswer.addEventListener("click", cmbTask3CheckAnswerClick);
 //--- Part 4 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
 
-function rdbCarNameSelect(aEvent){
+function rdbCarNameSelect(aEvent) {
   const txtTask4Output = document.getElementById("txtTask4Output");
   txtTask4Output.innerHTML = `User selected car type number: ${aEvent.target.value}`;
-  
 }
 
 const divTask4Cars = document.getElementById("divTask4Cars");
 
-for (let i = 0; i < CarTypes.length; i++){
-
+for (let i = 0; i < CarTypes.length; i++) {
   const car = CarTypes[i];
-  const inpRadio = document.createElement("input")
+  const inpRadio = document.createElement("input");
   inpRadio.type = "radio";
   inpRadio.name = "rdbCarName";
   inpRadio.value = car.value;
@@ -96,7 +144,6 @@ for (let i = 0; i < CarTypes.length; i++){
   divTask4Cars.appendChild(inpRadio);
   divTask4Cars.appendChild(lblCaption);
   divTask4Cars.appendChild(document.createElement("br"));
-
 }
 
 //--- Part 5 ----------------------------------------------------------------------------------------------
@@ -105,7 +152,7 @@ for (let i = 0; i < CarTypes.length; i++){
 const selectTask5Animals = document.getElementById("selectTask5Animals");
 const txtTask5Output = document.getElementById("txtTask5Output");
 
-function selectTask5AnimalsChange(){
+function selectTask5AnimalsChange() {
   const animalValue = selectTask5Animals.value;
   txtTask5Output.innerHTML = ` User selected animal nr: ${animalValue}`;
 }
@@ -116,14 +163,14 @@ selectTask5Animals.addEventListener("change", selectTask5AnimalsChange);
 
 const selectTask6Girls = document.getElementById("selectTask6Girls");
 const txtTask6Output = document.getElementById("txtTask6Output");
-for (let i = 0; i < GirlsNames.length; i++){
+for (let i = 0; i < GirlsNames.length; i++) {
   const option = document.createElement("option");
   option.value = i.toString();
   option.appendChild(document.createTextNode(GirlsNames[i]));
   selectTask6Girls.appendChild(option);
 }
 
-function selectGirlsChange(){
+function selectGirlsChange() {
   const value = parseInt(selectTask6Girls.value);
   const name = GirlsNames[value];
   txtTask6Output.innerHTML = `You selected: #${value}, ${name}`;
@@ -132,3 +179,56 @@ selectTask6Girls.addEventListener("change", selectGirlsChange);
 
 //--- Part 7 ----------------------------------------------------------------------------------------------
 /* Put your code below here!*/
+
+const txtMovieTitle = document.getElementById("txtMovieTitle");
+const selectMovieGenre = document.getElementById("selectMovieGenre");
+const txtMovieDirector = document.getElementById("txtMovieDirector");
+const txtMovieRate = document.getElementById("txtMovieRate");
+const cmbAddMovie = document.getElementById("cmbAddMovie");
+const tblMovies = document.getElementById("tblMovies");
+
+// Fill genre dropdown from MovieGenre array
+for (let i = 0; i < MovieGenre.length; i++) {
+  let opt = document.createElement("option");
+  opt.value = MovieGenre[i];
+  opt.appendChild(document.createTextNode(MovieGenre[i]));
+  selectMovieGenre.appendChild(opt);
+}
+
+function cmbAddMovieClick() {
+  let title = txtMovieTitle.value.trim();
+  let genre = selectMovieGenre.value;
+  let director = txtMovieDirector.value.trim();
+  let rate = txtMovieRate.value;
+
+  // Basic check
+  if (title === "" || director === "" || rate === "") {
+    alert("Please fill out all movie fields.");
+    return;
+  }
+
+  // Insert row at the end
+  let newRow = tblMovies.insertRow(-1);
+
+  // Count rows except the header row
+  let movieNumber = tblMovies.rows.length - 1;
+
+  // Create cells
+  let cellNr = newRow.insertCell(0);
+  let cellTitle = newRow.insertCell(1);
+  let cellGenre = newRow.insertCell(2);
+  let cellDirector = newRow.insertCell(3);
+  let cellRate = newRow.insertCell(4);
+
+  cellNr.innerText = movieNumber + ":";
+  cellTitle.innerText = title;
+  cellGenre.innerText = genre;
+  cellDirector.innerText = director;
+  cellRate.innerText = rate;
+
+  txtMovieTitle.value = "";
+  txtMovieDirector.value = "";
+  txtMovieRate.value = "5";
+}
+
+cmbAddMovie.addEventListener("click", cmbAddMovieClick);
